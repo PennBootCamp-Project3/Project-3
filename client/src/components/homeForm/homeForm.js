79aaea808591694aa9473d762b5ffa49d9659581
Doogle <fdunigan11@gmail.com>
2018-10-15 20:43:16 -0400

    Home page styling changed

--- client/src/components/homeForm/homeForm.js
+++ client/src/components/homeForm/homeForm.js
@@ -1,5 +1,13 @@
 import React, { Component } from "react";
-import { Card, Button, CardTitle, CardText, Col, Row } from "reactstrap";
+import {
+  Card,
+  Button,
+  CardTitle,
+  CardText,
+  CardImage,
+  CardBody,
+  CardGroup
+} from "mdbreact";
 import "./homeForm.css";
 
 export default class HomeForm extends Component {
@@ -11,93 +19,134 @@ export default class HomeForm extends Component {
         <div
           class="card card-image"
           style={{
-            backgroundImage: "url(" + "https://www.lvvwd.com/assets/images/about-us-pay-bill-16x7.jpg" + ")"
+            height: "500px",
+            backgroundImage:
+              "url(" +
+              "https://www.lvvwd.com/assets/images/about-us-pay-bill-16x7.jpg" +
+              ")"
           }}
         >
-          <div class="text-white text-center rgba-stylish-strong py-5 px-4">
+          <div class="text-white text-center rgba-stylish-strong py-5 px-4" style={{ height: "500px" }}>
             <div class="py-5">
-              <h2 class="card-title h2 my-4 py-2">
-                Welcome to ReactPay!
-              </h2>
-              <p class="mb-4 pb-2 px-md-5 mx-md-5">
-                ReactPay is an integration tool used by merchants 
-                to seamlessly streamline their payment processing 
-                systems.
+              <h1 class="card-title h1 my-4 py-2">Welcome to ReactPay!</h1>
+              <p class="mb-4 pb-1 px-md-2 mx-md-5">
+                ReactPay is an integration tool used by merchants to seamlessly
+                streamline their payment processing systems.
               </p>
             </div>
           </div>
         </div>
 
         <div>
-          <br />
-          <Row>
-            <Col md={6}>
-              <Card body inverse color="primary">
-                <CardTitle>Special Title Treatment</CardTitle>
+        <br />
+          <CardGroup deck>
+            <Card>
+              <CardImage
+                src="https://mdbootstrap.com/img/Photos/Others/images/49.jpg"
+                alt="Card image cap"
+                top
+                hover
+                overlay="white-slight"
+              />
+              <CardBody>
+                <CardTitle tag="h5">Setup Your Account</CardTitle>
                 <CardText>
-                  With supporting text below as a natural lead-in to additional
-                  content.
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
                 </CardText>
-                <Button href ="/pay/" color="indigo">Go to Pay Form</Button>
-              </Card>
-            </Col>
-            <Col md={6}>
-              <Card body inverse color="primary">
-                <CardTitle>Special Title Treatment</CardTitle>
+                <Button href="/pay/" color="primary">Go</Button>
+              </CardBody>
+            </Card>
+
+            <Card>
+              <CardImage
+                src="https://mdbootstrap.com/img/Photos/Others/images/48.jpg"
+                alt="Card image cap"
+                top
+                hover
+                overlay="white-slight"
+              />
+              <CardBody>
+                <CardTitle tag="h5">Choose Your Frequency</CardTitle>
                 <CardText>
-                  With supporting text below as a natural lead-in to additional
-                  content.
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
                 </CardText>
-                <Button href="/bill/" color="indigo">Go to Bill Form</Button>
-              </Card>
-            </Col>
-          </Row>
-          <br />
-          <Row>
-            <Col md={6}>
-              <Card body inverse color="info">
-                <CardTitle>Special Title Treatment</CardTitle>
+                <Button href="/bill/" color="primary">Go</Button>
+              </CardBody>
+            </Card>
+
+            <Card>
+              <CardImage
+                src="https://mdbootstrap.com/img/Photos/Others/images/77.jpg"
+                alt="Card image cap"
+                top
+                hover
+                overlay="white-slight"
+              />
+              <CardBody>
+                <CardTitle tag="h5">Get Your Reports</CardTitle>
                 <CardText>
-                  With supporting text below as a natural lead-in to additional
-                  content.
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
                 </CardText>
-                <Button href="/report/" color="indigo">Go to Report Form</Button>
-              </Card>
-            </Col>
-            <Col md={6}>
-              <Card body inverse color="info">
-                <CardTitle>Special Title Treatment</CardTitle>
+                <Button href="/report/" color="primary">Go</Button>
+              </CardBody>
+            </Card>
+            <Card>
+              <CardImage
+                src="https://mdbootstrap.com/img/Photos/Others/images/49.jpg"
+                alt="Card image cap"
+                top
+                hover
+                overlay="white-slight"
+              />
+              <CardBody>
+                <CardTitle tag="h5">A Nice to Have</CardTitle>
                 <CardText>
-                  With supporting text below as a natural lead-in to additional
-                  content.
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
                 </CardText>
-                <Button href="/bolt/" color="indigo">Go to Bolt Form</Button>
-              </Card>
-            </Col>
-          </Row>
-          <br />
-          <Row>
-            <Col md={6}>
-              <Card body inverse color="primary">
-                <CardTitle>Special Title Treatment</CardTitle>
+                <Button href="/bolt/" color="primary">Go</Button>
+              </CardBody>
+            </Card>
+
+            <Card>
+              <CardImage
+                src="https://mdbootstrap.com/img/Photos/Others/images/48.jpg"
+                alt="Card image cap"
+                top
+                hover
+                overlay="white-slight"
+              />
+              <CardBody>
+                <CardTitle tag="h5">Set Your Configuration</CardTitle>
                 <CardText>
-                  With supporting text below as a natural lead-in to additional
-                  content.
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
                 </CardText>
-                <Button href="/config/" color="indigo">Go to Config Form</Button>
-              </Card>
-            </Col>
-            <Col md={6}>
-              <Card body inverse color="primary">
-                <CardTitle>Special Title Treatment</CardTitle>
+                <Button href="/config/" color="primary">Go</Button>
+              </CardBody>
+            </Card>
+
+            <Card>
+              <CardImage
+                src="https://mdbootstrap.com/img/Photos/Others/images/77.jpg"
+                alt="Card image cap"
+                top
+                hover
+                overlay="white-slight"
+              />
+              <CardBody>
+                <CardTitle tag="h5">Logout of Your Account</CardTitle>
                 <CardText>
-                  With supporting text below as a natural lead-in to additional
-                  content.
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
                 </CardText>
-                <Button href="/" color="indigo">Logout</Button>
-              </Card>
-            </Col>
-          </Row>
+                <Button href="/" color="primary">Logout</Button>
+              </CardBody>
+            </Card>
+          </CardGroup>
         </div>
       </div>
     );
